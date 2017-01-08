2a14dc736ab8fc50db2b6631b924337c2049c9e5
Dushyant Min <dushyant@unotechsoft.com>
2017-01-08 20:47:50 +0530

    local repo file

---
+++ local_repo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -n "IP of the HTTP repo server:"
+read kk
+
+cat <<EOF >/etc/yum.repos.d/local.repo
+[LocalRepo]
+name=LocalRepo
+baseurl=http://$kk/rpms/common
+gpgcheck=0
+enabled=1
+priority=1
+EOF
+
+yum clean all
+
+yum reposlist
