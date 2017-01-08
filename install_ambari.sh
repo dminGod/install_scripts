f911a0e83217422d436987ebd78e0ba9c0ba562b
Dushyant Min <dushyant@unotechsoft.com>
2017-01-08 16:20:14 +0530

    Added ambari

---
+++ install_ambari.sh
@@ -0,0 +1,18 @@
+
+
+curl https://raw.githubusercontent.com/dminGod/install_java/master/install_java.sh | bash
+
+
+wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.1.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+
+yum install -y ambari-server
+
+
+echo "root:root" | chpasswd
+
+
+ambari-server -s setup
+
+ambari-server start
+
+ambari-server status
