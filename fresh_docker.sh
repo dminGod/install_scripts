f67b3eee1255e128ab0c4f025b0d44bfa89c1fec
Dushyant Min <dushyant@unotechsoft.com>
2017-01-08 22:26:04 +0530

    fresh docker 2

--- fresh_docker.sh
+++ fresh_docker.sh
@@ -1,4 +1,4 @@
 curl https://raw.githubusercontent.com/dminGod/install_scripts/master/local_repo.sh > local_repo.sh 
 bash local_repo.sh
-yum install openssh-* initscripts 
+yum install -y openssh-* initscripts 
 service sshd start
