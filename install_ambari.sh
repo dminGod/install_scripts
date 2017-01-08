6d0257128e3887767c84ee37d478adc891af65ae
Dushyant Min <dushyant@unotechsoft.com>
2017-01-08 16:21:25 +0530

    Added ambari 2

--- install_ambari.sh
+++ install_ambari.sh
@@ -1,6 +1,6 @@
 
 
-curl https://raw.githubusercontent.com/dminGod/install_java/master/install_java.sh | bash
+curl https://raw.githubusercontent.com/dminGod/install_scripts/master/install_java.sh | bash
 
 
 wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.1.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
